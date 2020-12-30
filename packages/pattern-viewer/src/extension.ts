0952c0a0410db3425df39432b000fb5037b400ac
shion <tenta_shion@yahoo.co.jp>
2020-12-30 17:27:02 +0900

    実装ブラッシュアップ

--- packages/pattern-viewer/src/extension.ts
+++ packages/pattern-viewer/src/extension.ts
@@ -13,16 +13,14 @@ export function activate(context: vscode.ExtensionContext) {
   // The command has been defined in the package.json file
   // Now provide the implementation of the command with registerCommand
   // The commandId parameter must match the command field in package.json
-  let disposable = vscode.commands.registerCommand('patternviewer.helloWorld', () => {
+  const commandName = 'patternviewer.helloWorld';
+  let disposable = vscode.commands.registerCommand(commandName, (...args: any[]) => {
     const panel = vscode.window.createWebviewPanel(
-      'patternviewer.helloWorld',
+      commandName,
       'PatternViewer',
       vscode.ViewColumn.One,
       {
-        enableScripts: true,
-        localResourceRoots: [
-          context.extensionUri
-        ]
+        enableScripts: true
       },
     );
 
